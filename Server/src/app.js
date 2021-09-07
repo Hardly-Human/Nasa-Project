ca23481bfe42f1052fcdde0844c71f3ec7acbe9d
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-09-07 12:34:09 +0530

    serving react-frontend from same production server using npm scripts

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
@@ -9,10 +10,12 @@ app.use(
 	})
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.use(planetsRouter);
 
 app.get("/", (req, res) => {
-	res.send("Basic Server Setup");
+	res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = app;
