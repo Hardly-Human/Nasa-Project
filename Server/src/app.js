360814cb32dbf575e70598d5ddf9c12dced92245
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-09-07 12:57:52 +0530

    Added logs to server using morgan middleware

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
@@ -9,6 +10,9 @@ app.use(
 		origin: "http://localhost:3000",
 	})
 );
+
+app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
