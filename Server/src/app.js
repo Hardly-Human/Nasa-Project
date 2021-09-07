99d43a3fafd0c86bab40aa24bc8a7f60dbc66b93
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-09-07 13:39:19 +0530

    Added GET /launches Endpoint

--- Server/src/app.js
+++ Server/src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 app.use(
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(planetsRouter);
+app.use(launchesRouter);
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "../public/index.html"));
